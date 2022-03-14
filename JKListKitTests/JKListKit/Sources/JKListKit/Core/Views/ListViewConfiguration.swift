9b0888d16d5afef063140027381dadc294ccffa3
javff <javff2@gmail.com>
2022-03-14 16:26:25 -0300

    Add documentation

--- JKListKitTests/JKListKit/Sources/JKListKit/Core/Views/ListViewConfiguration.swift
+++ JKListKitTests/JKListKit/Sources/JKListKit/Core/Views/ListViewConfiguration.swift
@@ -8,12 +8,21 @@
 import Foundation
 import UIKit
 
-public struct ContainerConfiguration {
+
+/// The object that sets the spacing and insets for the ListView.
+public struct ListViewConfiguration {
     public let spacing: CGFloat
     public let lateralSpacing: CGFloat
     public let topInset: CGFloat
     public let bottomInset: CGFloat
 
+
+    ///  Init of ContainerConfiguration
+    /// - Parameters:
+    ///   - spacing: default CGFloatZero. Define spacing into 
+    ///   - lateralSpacing: default is CGFloatZero. Spacing between sections.
+    ///   - topInset: default CGFloatZero. add additional scroll area in the top of the content
+    ///   - bottomInset: default CGFloatZero. add additional scroll area in the bottom of the content
     public init(spacing: CGFloat,
                  lateralSpacing: CGFloat = .zero,
                 topInset: CGFloat = .zero,
